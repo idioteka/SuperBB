f71a765484bd74bdb9c3f7a26c6e72cf34960406
Josip Maric <josip.maric@fiveminutes.eu>
2015-12-12 15:23:33 +0100

    Project created.

---
+++ SuperBBmap/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SuperBBmap
+//
+//  Created by Josip Maric on 30/11/15.
+//  Copyright © 2015 Josip Maric. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
